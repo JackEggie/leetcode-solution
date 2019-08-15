b26c42c43c923ea7cb092e7162d7f2bc4d29b486
Jack Tang <er_tzw@163.com>
2019-08-15 16:28:49 +0800

    Update Solution.java

--- src/main/java/name/jacktang/leetcode/_1/Solution.java
+++ src/main/java/name/jacktang/leetcode/_1/Solution.java
@@ -1,18 +1,14 @@
 package name.jacktang.leetcode._1;
 
-public class Solution {
-	public int[] twoSum(int[] nums, int target) {
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = 0; j < nums.length; j++) {
-				if (i == j) {
-					continue;
-				}
-
-				if (nums[i] + nums[j] == target) {
-					return new int[]{i, j};
-				}
-			}
-		}
-		return null;
-	}
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
 }
