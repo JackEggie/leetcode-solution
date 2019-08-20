b2de53beea4dccecaf44dafe37aa79599c91e68c
jtang1 <er_tzw@163.com>
2019-08-20 13:35:56 +0800

    add _532 solution

---
+++ src/main/java/name/jacktang/leetcode/_532/Solution.java
@@ -0,0 +1,36 @@
+package name.jacktang.leetcode._532;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+	public int findPairs(int[] nums, int k) {
+		Map<Integer, Integer> numCountMap = new TreeMap<>();
+		for (int num : nums) {
+			if (numCountMap.keySet().contains(num)) {
+				numCountMap.put(num, numCountMap.get(num) + 1);
+			} else {
+				numCountMap.put(num, 1);
+			}
+		}
+		int ret = 0;
+		if (k == 1) {
+			for (int count : numCountMap.values()) {
+				if (count > 1) {
+					ret++;
+				}
+			}
+			return ret;
+		} else {
+			Integer[] numArray = numCountMap.keySet().toArray(new Integer[]{});
+			for (int i = 0; i < numArray.length - 1; i++) {
+				for (int j = i + 1; j < numArray.length; j++) {
+					if (numArray[j] - numArray[i] == k) {
+						ret++;
+					}
+				}
+			}
+		}
+		return ret;
+	}
+}
