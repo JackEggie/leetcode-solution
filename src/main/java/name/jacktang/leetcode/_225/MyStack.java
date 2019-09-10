8f566a30c7f8d32548ef33b36cadd2c1fcf62fc8
jtang1 <er_tzw@163.com>
2019-09-10 16:20:28 +0800

    add _225 solution

---
+++ src/main/java/name/jacktang/leetcode/_225/MyStack.java
@@ -0,0 +1,36 @@
+package name.jacktang.leetcode._225;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+	private Queue<Integer> q = new LinkedList<>();
+
+	/** Initialize your data structure here. */
+	public MyStack() {
+	}
+
+	/** Push element x onto stack. */
+	public void push(int x) {
+		int size = q.size();
+		q.offer(x);
+		for (int i = 0; i < size; i++) {
+			q.offer(q.poll());
+		}
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	public int pop() {
+		return q.poll();
+	}
+
+	/** Get the top element. */
+	public int top() {
+		return q.peek();
+	}
+
+	/** Returns whether the stack is empty. */
+	public boolean empty() {
+		return q.isEmpty();
+	}
+}
